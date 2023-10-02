a575b21f8949de6e3753ed19819667a21ad90ee1
shaharshurki <shahar.shurki@gmail.com>
2023-10-02 13:57:00 +0300

    remove instance type var

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "region" {
   description = "The AWS region where resources will be created."
   type        = string
 }
-
-variable "instance_type" {
-  description = "The EC2 instance type for the web server."
-  type        = string
-}
